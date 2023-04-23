208e1a307cfbeb150b2595a5e036fdf8728fc60d
V-DUXY\Administrator <839724797@qq.com>
2023-04-23 15:29:04 +0800

    huxueapi

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using HuxueApi.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HuxueApi.Controllers
+{
+    [Route("Home")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        /// <summary>
+        /// 获取数据
+        /// </summary>
+        /// <param name="id">编码</param>
+        [HttpGet]
+        public Home GetHome(string id)
+        {
+            return (new Home());
+
+        }
+        /// <summary>
+        /// 添加数据
+        /// </summary>
+        [HttpPost]
+        public Home AddHomeInfo(Home home)
+        {
+            return (new Home());
+
+        }
+        /// <summary>
+        /// 更新数据
+        /// </summary>
+        [HttpPut]
+        public Home UpdateHomeInfo(Home home)
+        {
+            return (new Home());
+
+        }
+       /// <summary>
+       /// 删除数据
+       /// </summary>
+       /// <param name="id">编码</param>
+       /// <returns></returns>
+        [HttpDelete]
+        public Home DeleteHomeInfo(string id)
+        {
+            return (new Home());
+        }
+    }
+}
---
+++ Model/Home.cs
@@ -0,0 +1,9 @@
+﻿namespace HuxueApi.Model
+{
+    public class Home
+    {
+        public string HomeAddress { get; set; }
+        public string Car { get; set; }
+        public string Key { get; set; }
+    }
+}
